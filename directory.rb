8677908be95a0731a2cbf0dab0a1b040737dd002
Rick Clark <rkingsleyclark@gmail.com>
2016-12-02 11:29:06 +0000

    Added load from csv

--- directory.rb
+++ directory.rb
@@ -32,6 +32,7 @@ def print_menu
   puts "1. Input the students"
   puts "2. Show the students"
   puts "3. Save the list to students.csv"
+  puts "4. Load the list from students.csv"
   puts "9. Exit" # 9 because we'll be adding more items
 end
 
@@ -51,11 +52,23 @@ def process(selection)
       show_students
     when "3"
       save_students
+    when "4"
+      load_students
     when "9"
       exit # this will cause the program to terminate
     else
       puts "I don't know what you meant, try again"
   end
+end
+
+def load_students
+  file = File.open("students.csv", "r")
+  file.readlines.each {
+    |line|
+    name, cohort = line.chomp.split(",")
+    $students << { name: name, cohort: cohort.to_sym }
+  }
+  file.close
 end
 
 def save_students
